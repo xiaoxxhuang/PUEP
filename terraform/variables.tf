605594cf582ca20c35416b4ac22bd2300a487f1a
shuang <schuah@its.jnj.com>
2022-09-29 23:48:46 +0800

    PUEP: update variables.tf for ddb

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,15 +1,3 @@
-variable "project_name" {
-  description = "Name of the project. Must be unique."
-  type        = string
-  default     = "puep"
-}
-
-variable "environment" {
-  description = "Environment of the project."
-  type        = string
-  default  = "dev" 
-}
-
 variable "aws_region" {
   description = "AWS region"
   type        = string
