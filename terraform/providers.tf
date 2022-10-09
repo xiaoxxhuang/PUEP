8cf3637c797128e6cebd7bfddf1203395004eac9
Shuang <kimchichuah@hotmail.com>
2022-10-09 23:22:59 +0800

    PUEPdev: Remove unnecessary files

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,12 +1,3 @@
-# terraform {
-#   backend "s3" {
-#     bucket = "puep-state-s3-bucket"
-#     key    = "tf-workspaces/terraform.tfstate"
-#     region = "ap-southeast-1"
-#   }
-# }
-
 provider "aws" {
-  # profile = var.aws_profile
   region  = var.aws_region
 }
--- terraform/s3.tf
+++
@@ -1,3 +0,0 @@
-# resource "aws_s3_bucket" "puep_state_s3_bucket" {
-#   bucket = "${var.project_name}-${local.environment}-state-s3-bucket"
-# }
