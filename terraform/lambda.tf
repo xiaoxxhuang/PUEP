aa36aa0467ef0213ec96a241456822fdba836acd
Shuang <kimchichuah@hotmail.com>
2022-10-17 22:18:46 +0800

    lambda draft

---
+++ terraform/lambda.tf
@@ -0,0 +1,15 @@
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.module}/hello-world"
+  output_path = "${path.module}/hello-world.zip"
+}
+
+resource "aws_s3_object" "lambda_hello_world" {
+  bucket = "puep-dev"
+
+  key    = "hello-world.zip"
+  source = data.archive_file.lambda_hello_world.output_path
+
+  etag = filemd5(data.archive_file.lambda_hello_world.output_path)
+}
