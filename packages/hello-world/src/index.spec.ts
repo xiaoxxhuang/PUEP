71838f142127b40fe2c0177b01b63677153ee91f
shuang <schuah@its.jnj.com>
2022-10-31 11:34:10 +0800

    PUEPdev: testing on the new hello-world lambda function

--- packages/hello-world/src/index.spec.ts
+++ packages/hello-world/src/index.spec.ts
@@ -2,7 +2,7 @@ import { lambdaHandler } from "./index";
 import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
 
 describe("lambdaHandler", () => {
-  it("should return expected result", async() => {
+  it("should return expected result", async () => {
     const event = {
       body: {
         client_id: "client_id",
@@ -11,11 +11,14 @@ describe("lambdaHandler", () => {
     const context = {} as Context;
     const expectedResult: APIGatewayProxyResult = {
       statusCode: 200,
+      headers: {
+        "Content-Type": "application/json",
+      },
       body: JSON.stringify({
         message: "hello world",
       }),
-    }
+    };
     const result = await lambdaHandler(event, context);
     expect(result).toStrictEqual(expectedResult);
-  })
+  });
 });
