58d1011137338bedae6daddb9a4dffe7d07101b3
shuang <schuah@its.jnj.com>
2022-11-01 09:08:48 +0800

    PUEPdev: declare path in download artifact

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -1,7 +1,7 @@
 data "archive_file" "lambda_hello_world" {
   type = "zip"
 
-  source_dir  = "/home/runner/work/PUEP/PUEP/"
+  source_dir  = "../packages/hello-world/dist"
   output_path = "${path.module}/hello-world.zip"
 }
 
