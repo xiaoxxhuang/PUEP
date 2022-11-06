f607247565c9439168222820881114e66e03d819
Shuang <kimchichuah@hotmail.com>
2022-11-06 15:19:34 +0800

    PUEPdev: remove unnecessary changes

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -40,7 +40,7 @@ resource "aws_iam_role" "lambda_role" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
-      Action = ["sts:AssumeRole"]
+      Action = "sts:AssumeRole"
       Effect = "Allow"
       Sid    = ""
       Principal = {
