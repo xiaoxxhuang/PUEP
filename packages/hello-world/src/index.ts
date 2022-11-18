0b017d295b8107c9944228c506664301b1c5cd9e
shuang <schuah@its.jnj.com>
2022-11-18 15:22:02 +0800

    update route params

--- packages/hello-world/src/index.ts
+++ packages/hello-world/src/index.ts
@@ -5,19 +5,15 @@ import { pkmHandler } from "./pkms/handler";
 export const lambdaHandler = async (
   event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-  // const route = event.requestContext.resourceId;
-  // const queryParam = event.queryStringParameters;
-  
-  return {
-    statusCode: 200,
-    body: JSON.stringify(event)
+  const route = event.pathParameters?.PROXY;
+  const queryParam = event.queryStringParameters;
+
+  switch (route) {
+    case "emblems":
+      return await emblemHandler(queryParam);
+    case "pkms":
+      return await pkmHandler(queryParam);
+    default:
+      throw new Error(`Unsupported route: ${route}`);
   }
-  // switch (route) {
-  //   case "GET /puep/emblems":
-  //     return await emblemHandler(queryParam);
-  //   case "GET /puep/pkms":
-  //     return await pkmHandler(queryParam);
-  //   default:
-  //     throw new Error(`Unsupported route: ${route}`);
-  // }
 };
