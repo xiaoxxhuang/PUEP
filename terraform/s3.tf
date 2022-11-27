52a9c98839043a1a967e5b888c06ec07b1ba63c5
Shuang <kimchichuah@hotmail.com>
2022-11-27 18:35:51 +0800

    modify foreach  s3 react

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -52,8 +52,9 @@ resource "aws_s3_bucket_policy" "website_policy" {
 
 resource "aws_s3_object" "website_index" {
   bucket       = aws_s3_bucket.puep_website_s3_bucket.id
-  # for_each     = fileset("../packages/react-app/build/","*")
+  for_each     = fileset("../packages/react-app/build/","**/*.*")
   key          = "${each.value}"
-  source       = "../packages/react-app/build/"
+  source       = "../packages/react-app/build/${each.value}"
   acl          = "public-read"
+  etag   = filemd5("../packages/react-app/build/${each.value}")
 }
