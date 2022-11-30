7e296f74a53c1780b19e1037f17de580d05e87c7
Shuang <kimchichuah@hotmail.com>
2022-11-30 23:27:18 +0800

    remove redundant and tf apply

--- packages/react-app/src/App.tsx
+++ packages/react-app/src/App.tsx
@@ -8,16 +8,8 @@ function App() {
           Example of static website using react
         </p>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
           Learn React
-        </a>
+        </p>
       </header>
     </div>
   );
