3030b4e9ed654d827bef49aa7c598ef33cabccbf
Shuang <kimchichuah@hotmail.com>
2022-11-30 23:33:12 +0800

    add forwarded values for cloudfront

--- terraform/cloudfront.tf
+++ terraform/cloudfront.tf
@@ -1,4 +1,4 @@
-resource aws_cloudfront_distribution frontend {
+resource "aws_cloudfront_distribution" "frontend" {
   origin {
     domain_name = aws_s3_bucket.puep_website_s3_bucket.bucket_regional_domain_name
     origin_id   = "s3-${local.environment}"
@@ -16,6 +16,14 @@ resource aws_cloudfront_distribution frontend {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "s3-${local.environment}"
 
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+    
     viewer_protocol_policy = "redirect-to-https"
     min_ttl                = 0
     default_ttl            = 3600
