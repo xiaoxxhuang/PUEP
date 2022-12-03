b4361c7376664f778ebf2677ec943d11cf154493
shuang <schuah@its.jnj.com>
2022-12-03 11:42:03 +0800

    remove s3 public read

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -7,14 +7,13 @@ resource "aws_s3_bucket_acl" "puep_s3_bucket_acl" {
   acl    = var.acl_value
 }
 
-# ------------------------------------------------------------------------ 
 resource "aws_s3_bucket" "puep_website_s3_bucket" {
   bucket = "${var.project_name}-${local.environment}-website-s3-bucket"
 }
 
 resource "aws_s3_bucket_acl" "puep_website_s3_bucket_acl" {
   bucket = aws_s3_bucket.puep_website_s3_bucket.id
-  acl    = "public-read"
+  acl    = var.acl_value
 }
 
 resource "aws_s3_bucket_versioning" "website_versioning" {
@@ -32,30 +31,12 @@ resource "aws_s3_bucket_website_configuration" "website_config" {
   }
 }
 
-resource "aws_s3_bucket_policy" "website_policy" {
-  bucket = aws_s3_bucket.puep_website_s3_bucket.id
-  policy = jsonencode(
-    {
-      "Version" : "2012-10-17",
-      "Statement" : [
-        {
-          "Sid" : "PublicReadGetObject",
-          "Effect" : "Allow",
-          "Principal" : "*",
-          "Action" : "s3:GetObject",
-          "Resource" : "arn:aws:s3:::${aws_s3_bucket.puep_website_s3_bucket.id}/*"
-        }
-      ]
-    }
-  )
-}
-
 resource "aws_s3_object" "website_index" {
   bucket       = aws_s3_bucket.puep_website_s3_bucket.id
   for_each     = fileset("../packages/react-app/build/","**/*.*")
   key          = "${each.value}"
   source       = "../packages/react-app/build/${each.value}"
-  acl          = "public-read"
+  acl          = var.acl_value
   content_type = lookup(local.mimes, element(reverse(split(".", each.value)), 0), "")
-  etag   = filemd5("../packages/react-app/build/${each.value}")
+  etag         = filemd5("../packages/react-app/build/${each.value}")
 }
