02060bc6d8e8a046e2703e74245924c0e4ab45ca
shuang <schuah@its.jnj.com>
2022-12-13 22:03:53 +0800

    remove console.log

--- packages/react-app/src/button/index.test.tsx
+++ packages/react-app/src/button/index.test.tsx
@@ -5,18 +5,16 @@ describe("Render Button", () => {
   describe("Render Filter Button", () => {
     it("Should display with text `Filter`, have class `Button`", () => {
       render(<Button children="Filter" className="Button" disabled={false} />);
-      const buttonElement = screen.getByRole('button', { name: /Filter/i});
-      console.log(buttonElement)
+      const buttonElement = screen.getByRole("button", { name: /Filter/i });
       expect(buttonElement).toBeInTheDocument();
       expect(buttonElement).toHaveClass("Button");
       expect(buttonElement).not.toBeDisabled();
       expect(buttonElement.firstChild).toHaveTextContent("Filter");
-      
     });
 
     it("Should display with text `Filter`, have class `Button`, and disabled when user clicked on it", () => {
       render(<Button children="Filter" disabled={true} className="Button" />);
-      const buttonElement = screen.getByRole('button', { name: /Filter/i});
+      const buttonElement = screen.getByRole("button", { name: /Filter/i });
       expect(buttonElement).toBeInTheDocument();
       expect(buttonElement).toHaveClass("Button");
       expect(buttonElement).toBeDisabled();
