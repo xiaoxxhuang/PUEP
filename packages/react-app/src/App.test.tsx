cd8c18f503b5dbaad67ce3b4306919abcaa6314b
shuang <schuah@its.jnj.com>
2022-12-21 16:52:15 +0800

    App.tsx test files

--- packages/react-app/src/App.test.tsx
+++ packages/react-app/src/App.test.tsx
@@ -1,13 +1,18 @@
-import { render, screen, fireEvent } from '@testing-library/react';
-import App from './App';
+import { render, screen, fireEvent } from "@testing-library/react";
+import App from "./App";
 
-describe('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("renders learn react link", () => {
+  beforeEach(() => {
+    render(<App />);
+  });
 
-  it("calls the onChange handler when a radio button is selected", () => {
-    const radioButton2: HTMLInputElement = screen.getByLabelText('Attack');
+  it("Should render the App element", () => {
+    const linkElement = screen.getByText(/learn react/i);
+    expect(linkElement).toBeInTheDocument();
+  });
+
+  it("Should check a radio button when the radio button is selected", () => {
+    const radioButton2: HTMLInputElement = screen.getByLabelText("Attack");
     fireEvent.click(radioButton2);
     expect(radioButton2.checked).toEqual(true);
   });
